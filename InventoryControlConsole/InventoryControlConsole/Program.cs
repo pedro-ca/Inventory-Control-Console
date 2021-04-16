6b94fd96f71bebb80bbf04aa97bbed8e7f1fe224
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-04-16 15:00:39 -0300

    ViewEquipments and ViewMaintenanceCalls methods

--- InventoryControlConsole/InventoryControlConsole/Program.cs
+++ InventoryControlConsole/InventoryControlConsole/Program.cs
@@ -10,6 +10,41 @@ namespace InventoryControlConsole
 {
     class Program
     {
+        private static Equipment[] equipmentArray = new Equipment[20];
+        private static MaintenanceCall[] maintenanceCallArray = new MaintenanceCall[20];
+
+        private static void ViewEquipments()
+        {
+            for(int i=0; i < equipmentArray.Length; i++)
+            {
+               if(equipmentArray[i] != null)
+                {
+                    Console.WriteLine($"Equipment {i}:");
+                    Console.WriteLine("   -EquipmentName = " + equipmentArray[i].EquipmentName);
+                    Console.WriteLine("   -AcquisitionPrice = " + equipmentArray[i].AcquisitionPrice);
+                    Console.WriteLine("   -SerialNumber = " + equipmentArray[i].SerialNumber);
+                    Console.WriteLine("   -ManufacturingDate = " + equipmentArray[i].ManufacturingDate);
+                    Console.WriteLine("   -ManufacturerName = " + equipmentArray[i].ManufacturerName);
+                    Console.WriteLine("   -Equipment name = " + equipmentArray[i].EquipmentName);
+                }
+            }
+        }
+
+        private static void ViewMaintenanceCalls()
+        {
+            for (int i = 0; i < maintenanceCallArray.Length; i++)
+            {
+                if (maintenanceCallArray[i] != null)
+                {
+                    Console.WriteLine($"Maintenance Call {i}:");
+                    Console.WriteLine("   -TitleName = " + maintenanceCallArray[i].TitleName);
+                    Console.WriteLine("   -DescriptioName = " + maintenanceCallArray[i].DescriptioName);
+                    Console.WriteLine("   -Equipment = " + maintenanceCallArray[i].Equipment.EquipmentName);
+                    Console.WriteLine("   -OpeningDate = " + maintenanceCallArray[i].OpeningDate);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
 
