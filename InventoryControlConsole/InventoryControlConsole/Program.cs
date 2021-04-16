29f093557121f5637a9d954e3da2f4e031aed14f
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-04-16 14:34:10 -0300

    Added InventoryControlModel classes

--- InventoryControlConsole/InventoryControlConsole/Program.cs
+++ InventoryControlConsole/InventoryControlConsole/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using InventoryControlModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace InventoryControlConsole
 {
@@ -10,6 +12,7 @@ namespace InventoryControlConsole
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
